66d641899f3791bd983cf4244210c29f134cba1a
leechanwoo <cjswosa22@naver.com>
2017-02-10 18:22:59 +0900

    check point added

--- TensorflowOnAir/NeuralNetExamples/RNN_Example.py
+++ TensorflowOnAir/NeuralNetExamples/RNN_Example.py
@@ -1,62 +1,86 @@
 
-import os
+"""
+* sin learned RNN model
+"""
+
 import tensorflow as tf
 
-CONST = tf.app.flags
-CONST.DEFINE_integer("epoch", 1000, "epoch when learning")
-CONST.DEFINE_integer("samples", 1000, "number of samples for learning")
-CONST.DEFINE_integer("state_size", 5, "state size in rnn ")
-CONST.DEFINE_integer("recurrent", 5, "number of recurrent hidden layer")
-CONST.DEFINE_integer("input_vector_size", 1, "input vector size")
-CONST.DEFINE_float("learning_rate", 0.001, "learning rate for optimizer")
-CONST = CONST.FLAGS
+CONSTANT = tf.app.flags
+CONSTANT.DEFINE_integer("epoch", 1000, "epoch when learning")
+CONSTANT.DEFINE_integer("samples", 1000, "number of samples for learning")
+CONSTANT.DEFINE_integer("state_size", 5, "state size in rnn ")
+CONSTANT.DEFINE_integer("recurrent", 5, "number of recurrent hidden layer")
+CONSTANT.DEFINE_integer("input_vector_size", 1, "input vector size")
+CONSTANT.DEFINE_float("learning_rate", 0.001, "learning rate for optimizer")
+CONST = CONSTANT.FLAGS
 
 class RNN(object):
     """
      * RNN model
     """
-    def __init__(self):
+    def __init__(self, train=True):
+        self.is_train = train
+        self._to_plot()
         self._gen_sim_data()
         self._build_batch()
         self._set_variables()
         self._build_model()
         self._save_model()
-        self._build_train()
-        self._initialize()
+        if self.is_train is True:
+            self._build_train()
+        else:
+            pass
+        self._initialize(train)
 
 
     def run(self):
         """
          * run the rnn model
         """
-        for step in xrange(CONST.epoch):
-            loss = self._run()
-            if step % 100 == 0:
-                self._write_checkpoint(os.getcwd() + "/checkpoint/rnn_learned_in_sine.ckpt")
-                print "model saved..."
-                print "loss: ", loss
 
-        #self._line_plot_draw()
+        for step in xrange(CONST.epoch):
+            if self.is_train is True:
+                loss = self._run_train(step)
+                if step % 100 == 0:
+                    self._write_checkpoint("./NeuralNetExamples/checkpoint/rnn.ckpt")
+                    print "model saved..."
+                    print "loss: ", loss
+            else:
+                self._run_pred(step)
+                self._line_plot_draw()
+
         print "training done"
         self._close_session()
 
     @classmethod
-    def _run(cls):
-        _, loss = cls.sess.run([cls.train, cls.loss])
+    def _run_train(cls, step):
+        _, loss = cls.sess.run([cls.train, cls.loss], feed_dict={cls.idx: step})
         return loss
 
     @classmethod
-    def _write_checkpoint(cls, directory):
-        cls.saver.save(cls.sess, directory)
+    def _run_pred(cls, step):
+        print cls.sess.run([tf.shape(cls.pred), cls.sz_y])
+        return cls.sess.run(cls.pred, feed_dict={cls.idx: step})
 
     @classmethod
     def _save_model(cls):
         cls.saver = tf.train.Saver()
 
     @classmethod
-    def _initialize(cls):
+    def _write_checkpoint(cls, directory):
+        cls.saver.save(cls.sess, directory)
+
+    @classmethod
+    def _restore_checkpoint(cls, directory):
+        cls.saver.restore(cls.sess, directory)
+
+    @classmethod
+    def _initialize(cls, train):
         cls.sess = tf.Session()
-        cls.sess.run(tf.global_variables_initializer())
+        if train is True:
+            cls.sess.run(tf.global_variables_initializer())
+        else:
+            cls._restore_checkpoint("./NeuralNetExamples/checkpoint/rnn.ckpt")
         cls.coord = tf.train.Coordinator()
         cls.thread = tf.train.start_queue_runners(cls.sess, cls.coord)
 
@@ -70,6 +94,7 @@ class RNN(object):
     def _gen_sim_data(cls):
         ts_x = tf.constant(range(CONST.samples+1), dtype=tf.float32)
         ts_y = tf.sin(ts_x*0.1)
+        cls.sz_y = tf.shape(ts_y)
         cls._line_plot("sin_x", ts_y, CONST.samples+1)
 
         sz_batch = (CONST.samples/CONST.state_size, CONST.state_size, CONST.input_vector_size)
@@ -99,6 +124,7 @@ class RNN(object):
         cls.batch_label = tf.unpack(cls.batch_label, axis=1)
         cls.output, _ = tf.nn.rnn(rnn_cell, cls.input_set, dtype=tf.float32)
         cls.pred = tf.matmul(cls.output, cls.linear_w) + cls.linear_b
+        cls._line_plot("pred", cls.pred, 1)
 
     @classmethod
     def _build_train(cls):
@@ -115,8 +141,9 @@ class RNN(object):
     @classmethod
     def _line_plot(cls, name, tensor, length):
         cls.length = length
-        cls.idx = tf.placeholder(tf.int32)
-        cls.plot = tensor[cls.idx]
+        cls.plot = tf.cond(tf.rank(tensor) > 1,
+        lambda: tf.pack(tensor, 1)[cls.idx],
+        lambda: tensor[cls.idx])
         tf.summary.scalar(name, cls.plot)
 
     @classmethod
@@ -129,12 +156,17 @@ class RNN(object):
             writer.flush()
         writer.close()
 
+    @classmethod
+    def _to_plot(cls):
+        cls.idx = tf.placeholder(tf.int32)
+
 def main(_):
     """
     * code start here
     """
-    rnn = RNN()
+    rnn = RNN(False)
     rnn.run()
+    print "end process"
 
 if __name__ == "__main__":
     tf.app.run()
