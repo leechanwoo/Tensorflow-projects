7f6fa919a84b7bc5aa3c0761f33fad0b45f9ef41
leechanwoo <cjswosa22@naver.com>
2017-08-08 17:27:26 +0900

    column correced

--- tests/test.py
+++ tests/test.py
@@ -9,8 +9,9 @@ data = tf.contrib.data.TextLineDataset("./tests/test2.csv").skip(1).batch(10)
 itr = data.make_one_shot_iterator()
 batch = itr.get_next()
 
-col0, col1, col2, col3, col4 = tf.decode_csv(batch, record_defaults=[[0]]*5)
-batch = tf.stack([col0, col1, col2, col3, col4], axis=1)
+cols = tf.decode_csv(batch, record_defaults=[[0]]*5)
+
+batch = tf.stack(cols, axis=1)
 
 with tf.Session() as sess:
     for i in range(10):
